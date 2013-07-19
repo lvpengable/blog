c035c315e9278c8d17efcaa4f6e99407dddc88b8
lvp <lvp@lvp-945GCM-S2C.(none)>
2013-07-19 02:56:53 -0400

    new blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a4a2464e3200a9019a99b29679faa32cae7a6583f2b7844df92afabb2cf49bd5c7a43b4293a97b0aac61958ab9a9df8eee10bb2bf067f98be58bde09e2745faf'
